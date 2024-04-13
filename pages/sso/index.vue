bdd7635e8cef4ee8a8897a1b11eb2424e64f5b5f
youfeed <11247005@qq.com>
2024-04-14 00:17:49 +0800

    add

--- pages/sso/index.vue
+++ pages/sso/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sso" fixed top-0 left-0 w-full h-full flex items-center justify-center>
     <div class="layout" bg-white rounded shadow-lg m-2>
-
+      <div>loading...</div>
     </div>
   </div>
 </template>
@@ -15,15 +15,11 @@ import { computed, markRaw, onMounted, reactive, toRefs } from "vue";
 const state = reactive({
   name:'youloge.sso',
   version:'v1.1.4',
-  hash:location.hash,
   referrer:document.referrer,
   uage:navigator.language.toLowerCase(),
-  mask:false,
-  close:true
 });
 onMounted(()=>{
   let Params = new URL(location.href).searchParams
-  let hash = Params.get('hash')
   let method = Params.get('state')
   let params = Params.get('code')
   if(method && params){
